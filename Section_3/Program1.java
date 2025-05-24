218db841992c5cb0f96cf1ba254b375133e1d85b
Riyanshu Sharma <riyanshu062@gmail.com>
2025-05-24 21:39:38 +0530

    h

--- Section_3/Program1.java
+++ Section_3/Program1.java
@@ -2,6 +2,7 @@ package Section_3;
 
 public class Program1 {
     public static void main(String[] args) {
-        System.out.println();
+        
+        System.out.println("Hello");
     }
 }
--- Section_4/Program1.java
+++
@@ -1,9 +0,0 @@
-package Section_4;
-
-public class Program1 {
-    
-    public static void main(String[] args) {
-        
-        System.out.println("hgfj");
-    }
-}
