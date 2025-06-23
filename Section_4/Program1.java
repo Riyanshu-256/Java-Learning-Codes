42282c7498a5d396b71c08b9cd24458937621f3c
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-24 00:07:39 +0530

    Hello,Tim

--- Section_4/Program1.java
+++ Section_4/Program1.java
@@ -2,7 +2,6 @@ package Section_4;
 
 public class Program1 {
     public static void main(String[] args) {
-        System.out.print("Hello World\n");
         System.out.print("Hello, Tim");
     }
 }
