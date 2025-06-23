e699e9c6e37a48296b4bb1288dce7816c0fb1c55
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-24 00:47:45 +0530

    Ternary Operator

--- Section_4/P03_Logic.java
+++ Section_4/P03_Logic.java
@@ -25,8 +25,17 @@ public class P03_Logic {
             System.out.println("This is true");
         }
         boolean isCar = false;
-        if (isCar == true) {
+        if (!isCar) {
             System.out.println("This is not supposed to happen");
         }
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if(isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
     }
 }
