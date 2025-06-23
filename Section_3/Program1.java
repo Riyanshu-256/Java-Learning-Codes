51c46d12d2e111de3c1d4ffd7b710557e46074ec
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-23 22:15:36 +0530

    Section 3

--- Section_3/Program1.java
+++ Section_3/Program1.java
@@ -3,6 +3,6 @@ package Section_3;
 public class Program1 {
     public static void main(String[] args) {
         
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
---
+++ Section_3/Program2.java
@@ -0,0 +1,12 @@
+package Section_3;
+
+
+public class Program2 {
+    int myFirstNumber = 5;
+
+   System.out.print("myFirstNumber");
+   System.out.print(myFirstNumber);
+
+
+    
+}
