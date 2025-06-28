bee9f077fbdd951ffea8ca8e6a65b2461d427ac1
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-29 03:18:55 +0530

    Composition2

--- Section_8/P01_COMPOSITION/Main.java
+++ Section_8/P01_COMPOSITION/Main.java
@@ -1,5 +1,7 @@
 package Section_8.P01_COMPOSITION;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        ComputerCase = new ComputerCase("2208", "Dell", "240");
+    }
 }
--- Section_8/P01_COMPOSITION/Personal_Computer.java
+++ Section_8/P01_COMPOSITION/Personal_Computer.java
@@ -1,5 +1,38 @@
 package Section_8.P01_COMPOSITION;
 
-public class Personal_Computer {
-    privat
+public class Personal_Computer extends Product {
+
+    private ComputerCase computerCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public Personal_Computer(String model, String manufacturer,
+        ComputerCase computerCase, Monitor monitor,
+        Motherboard motherboard) {
+        super(model, manufacturer);
+        this.computerCase = computerCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    private void drawLogo() {
+        monitor.drawPixelAt(1200, 50, "yellow");
+    }
+
+    public void powerUp() {
+        computerCase.pressPowerButton();
+        drawLogo();
+    }
+
+//    public ComputerCase getComputerCase() {
+//        return computerCase;
+//    }
+//
+//    public Monitor getMonitor() {
+//        return monitor;
+//    }
+//
+//    public Motherboard getMotherboard() {
+//        return motherboard;
+//    }
 }
