a0bf487004f15ee2058d726a90335839bb4e60e6
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-30 20:34:49 +0530

    main

--- Section_8/P01_COMPOSITION/Main.java
+++ Section_8/P01_COMPOSITION/Main.java
@@ -1,7 +1,18 @@
-package Section_8.P01_COMPOSITION;
+ Section_8.P01_COMPOSITION;
 
 public class Main {
-    public static void main(String[] args) {
-        ComputerCase = new ComputerCase("2208", "Dell", "240");
-    }
+   ComputerCase theCase = new ComputerCase("2208", "Dell",
+                "240");
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer",
+                27, "2540 x 1440");
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus",
+                4, 6, "v2.44");
+        Personal_Computer thePC = new Personal_Computer("2208", "Dell",
+      package          theCase, theMonitor, theMotherboard);
+
+//        thePC.getMonitor().drawPixelAt(10, 10, "red");
+//        thePC.getMotherboard().loadProgram("Windows OS");
+//        thePC.getComputerCase().pressPowerButton();
+
+    thePC.powerUp();
 }
