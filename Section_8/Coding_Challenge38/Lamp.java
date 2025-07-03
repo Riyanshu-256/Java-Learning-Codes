275915e85aef0f939ec4cb2ffbbbd3a5d94c06e9
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-04 01:06:19 +0530

    Challenge38 Completed

--- Section_8/Coding_Challenge38/Lamp.java
+++ Section_8/Coding_Challenge38/Lamp.java
@@ -2,4 +2,29 @@ package Section_8.Coding_Challenge38;
 
 public class Lamp {
     
+    private String style;
+    private boolean battery;
+    private int globRating;
+ 
+    public Lamp(String style, boolean battery, int globRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globRating = globRating;
+    }
+ 
+    public void turnOn() {
+        System.out.print("Lamp -> Turning on");
+    }
+ 
+    public String getStyle() {
+        return style;
+    }
+ 
+    public boolean isBattery() {
+        return battery;
+    }
+ 
+    public int getGlobRating() {
+        return globRating;
+    }
 }
--- Section_8/Coding_Challenge38/Wall.java
+++ Section_8/Coding_Challenge38/Wall.java
@@ -2,4 +2,13 @@ package Section_8.Coding_Challenge38;
 
 public class Wall {
     
+    private String direction;
+ 
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+ 
+    public String getDirection() {
+        return direction;
+    }
 }
