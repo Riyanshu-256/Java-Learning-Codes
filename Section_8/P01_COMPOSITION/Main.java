e452c0321d0a9677eac80c15128778fb7ad7ee3a
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-04 00:28:18 +0530

    Main

--- Section_8/P01_COMPOSITION/Main.java
+++ Section_8/P01_COMPOSITION/Main.java
@@ -1,30 +1,30 @@
-package Section_8.P01_COMPOSITION;
+// package Section_8.P01_COMPOSITION;
 
-public class Main {
-   public static void main(String[] args) {
+// public class Main {
+//    public static void main(String[] args) {
         
-           // Create an object of type computer case
-   ComputerCase theCase = new ComputerCase("2208", "Dell",
-                "240");
+//            // Create an object of type computer case
+//    ComputerCase theCase = new ComputerCase("2208", "Dell",
+//                 "240");
 
-        // Create an object of type monitor case
-        Monitor theMonitor = new Monitor("27inch Beast", "Acer",
-                27, "2540 x 1440");
+//         // Create an object of type monitor case
+//         Monitor theMonitor = new Monitor("27inch Beast", "Acer",
+//                 27, "2540 x 1440");
 
-        // Create an object of type motherboard case
-        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus",
-                4, 6, "v2.44");
+//         // Create an object of type motherboard case
+//         Motherboard theMotherboard = new Motherboard("BJ-200", "Asus",
+//                 4, 6, "v2.44");
 
-        // Create an object of type personal computer case
-        Personal_Computer thePC = new Personal_Computer("2208", "Dell",
-                theCase, theMonitor, theMotherboard);
+//         // Create an object of type personal computer case
+//         Personal_Computer thePC = new Personal_Computer("2208", "Dell",
+//                 theCase, theMonitor, theMotherboard);
 
-        // // To call the get => monitor, motherboard & computer case method
-        // thePC.getMonitor().drawPixelAt(10, 10, "red");
-        // thePC.getMotherboard().loadProgram("Windows OS");
-        // thePC.getComputerCase().pressPowerButton();
+//         // // To call the get => monitor, motherboard & computer case method
+//         // thePC.getMonitor().drawPixelAt(10, 10, "red");
+//         // thePC.getMotherboard().loadProgram("Windows OS");
+//         // thePC.getComputerCase().pressPowerButton();
 
-        thePC.powerUp();
-   }
+//         thePC.powerUp();
+//    }
 
-}
+// }
--- Section_8/P01_COMPOSITION/Personal_Computer.java
+++ Section_8/P01_COMPOSITION/Personal_Computer.java
@@ -1,39 +1,40 @@
-package Section_8.P01_COMPOSITION;
+// package Section_8.P01_COMPOSITION;
 
-public class Personal_Computer extends Product {
+// public class Personal_Computer extends Product {
 
-    private ComputerCase computerCase;
-    private Monitor monitor;
-    private Motherboard motherboard;
+//     private ComputerCase computerCase;
+//     private Monitor monitor;
+//     private Motherboard motherboard;
 
-    public Personal_Computer(String model, String manufacturer,
+//     public Personal_Computer(String model, String manufacturer,
 
-        ComputerCase computerCase, Monitor monitor,
-        Motherboard motherboard) {
-        super(model, manufacturer);
-        this.computerCase = computerCase;
-        this.monitor = monitor;
-        this.motherboard = motherboard;
-    }
+//         ComputerCase computerCase, Monitor monitor,
+//         Motherboard motherboard) {
+//         super(model, manufacturer);
+//         this.computerCase = computerCase;
+//         this.monitor = monitor;
+//         this.motherboard = motherboard;
+//     }
 
-    private void drawLogo() {
-        monitor.drawPixelAt(1200, 50, "yellow");
-    }
+//     private void drawLogo() {
+//         monitor.drawPixelAt(1200, 50, "yellow");
+//     }
 
-    public void powerUp() {
-        computerCase.pressPowerButton();
-        drawLogo();
-    }
+//     public void powerUp() {
+//         computerCase.pressPowerButton();
+//         drawLogo();
+//     }
 
-//    public ComputerCase getComputerCase() {
-//        return computerCase;
-//    }
-//
-//    public Monitor getMonitor() {
-//        return monitor;
-//    }
-//
-//    public Motherboard getMotherboard() {
-//        return motherboard;
-//    }
-}
+// //    public ComputerCase getComputerCase() {
+// //        return computerCase;
+// //    }
+// //
+// //    public Monitor getMonitor() {
+// //        return monitor;
+// //    }
+// //
+// //    public Motherboard getMotherboard() {
+// //        return motherboard;
+// //    }
+// }
+
