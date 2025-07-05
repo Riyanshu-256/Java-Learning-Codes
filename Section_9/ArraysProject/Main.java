a13a1418896f27a84991a2d18865be92b5b78897
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-05 18:02:17 +0530

    Main

--- Section_9/ArraysProject/Main.java
+++ Section_9/ArraysProject/Main.java
@@ -40,13 +40,13 @@ public class Main {
         System.out.println();
         System.out.println(newArray);
         System.out.println(Arrays.toString(newArray));
-        if (objectVariable instanceof int[]) {
-            System.out.println("objectVariable is really an int array");
-        }
-        Object[] objectArray = new Object[3];
-        objectArray[0] = "Hello";
-        objectArray[1] = new StringBuilder("World");
-        objectArray[2] = newArray;
+    //     if (objectVariable instanceof int[]) {
+    //         System.out.println("objectVariable is really an int array");
+    //     }
+    //     Object[] objectArray = new Object[3];
+    //     objectArray[0] = "Hello";
+    //     objectArray[1] = new StringBuilder("World");
+    //     objectArray[2] = newArray;
 
-    }
+     }
 }
